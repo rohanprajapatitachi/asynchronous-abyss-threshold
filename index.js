4f4251852b07706bcaae8305e6a3969e8e787f3e
rohanprajapati <rohanprajapati369@gmail.com>
2024-09-13 09:11:08 +0545

    Array Fundamentals

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 console.log(" Chai or Code !!!");
 
 // const string = require("./es6-and-beyond/string");
-require("./es6-and-beyond/nums_math_dates");
+// require("./es6-and-beyond/nums_math_dates");
+require("./es6-and-beyond/arrays");
 
