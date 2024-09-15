24afa1f401997102361558c23a280184e2e89ad6
rohanprajapati <rohanprajapati369@gmail.com>
2024-09-15 22:07:14 +0545

    Add examples demonstrating Array.prototype.slice() usage

--- es6-and-beyond/arrays.js
+++ es6-and-beyond/arrays.js
@@ -1,3 +1,17 @@
 const myArray = [2, 4, 6, 8 ,10, 12, 14, 16];
+console.log(myArray.slice(3, 5)); // [ 8, 10 ]
 
-console.log(myArray);
+// Array.prototype.slice()
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+console.log(animals.slice(2));
+// Expected output: Array ["camel", "duck", "elephant"]
+console.log(animals.slice(2, 4));
+// Expected output: Array ["camel", "duck"]
+console.log(animals.slice(1, 5));
+// Expected output: Array ["bison", "camel", "duck", "elephant"]
+console.log(animals.slice(-2));
+// Expected output: Array ["duck", "elephant"]
+console.log(animals.slice(2, -1));
+// Expected output: Array ["camel", "duck"]
+console.log(animals.slice());
+// Expected output: Array ["ant", "bison", "camel", "duck", "elephant"]
