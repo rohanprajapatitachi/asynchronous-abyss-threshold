3f601f93f5726872abc41a8ee0f881a93743ddad
rohanprajapati <rohanprajapati369@gmail.com>
2024-09-23 22:05:39 +0545

    spread operator

--- es6-and-beyond/arrays.js
+++ es6-and-beyond/arrays.js
@@ -16,13 +16,13 @@ console.log(animals.slice(2, -1));
 console.log(animals.slice());
 // Expected output: Array ["ant", "bison", "camel", "duck", "elephant"]
 
+
 // Array.prototype.splice()
 // splice(start)
 // splice(start, deleteCount)
 // splice(start, deleteCount, item1)
 // splice(start, deleteCount, item1, item2)
 // splice(start, deleteCount, item1, item2, /* …, */ itemN)
-
 const months = ['Jan', 'March', 'April', 'June'];
 months.splice(1, 0, 'Feb');
 // Inserts at index 1
@@ -48,6 +48,7 @@ console.log(pets.includes('elephant'));
 myArray.push(18)
 console.log(myArray);
 
+
 // Array.prototype.pop()
 const plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato'];
 console.log(plants.pop());
@@ -57,4 +58,38 @@ console.log(plants);
 plants.pop();
 console.log(plants);
 // Expected output: Array ["broccoli", "cauliflower", "cabbage"]
+
+
+// Array.prototype.concat()
+// concat()
+// concat(value1)
+// concat(value1, value2)
+// concat(value1, value2, /* …, */ valueN)
+const array11 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+const array3 = array11.concat(array2);
+console.log(array3);
+// Expected output: Array ["a", "b", "c", "d", "e", "f"]
+
+
+// SPREAD OPERATOR
+const myArray1 = [200, 300];
+const myArray2 = [400, 500];
+const su = [...myArray1, ...myArray2];
+console.log(su);
+
+function sum(x, y, z) {
+    return x + y + z;
+  } 
+  const numbers = [1, 2, 3];
+  console.log(sum(...numbers));
+  // Expected output: 6
+  console.log(sum.apply(null, numbers));
+  // Expected output: 6
+  
+
+
+
+
+
 
